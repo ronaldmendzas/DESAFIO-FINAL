4849c65c61f27a455f7ea06737071c1a33a964d7
Ronald Mendoza <ronaldmencas@gmail.com>
2026-05-28 13:40:04 -0400

    fix: dot pattern visible - remove bg-bg covering dots, increase opacity to 25%

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -5,22 +5,24 @@ import { DotPattern } from '@/components/ui/dot-pattern'
 
 export function AppLayout() {
   return (
-    <div className="min-h-screen bg-bg text-text relative">
+    <div className="min-h-screen text-text relative">
       <DotPattern
         width={16}
         height={16}
         cr={1}
         glow
-        className="absolute inset-0 text-forest/[0.15] -z-10"
+        className="absolute inset-0 text-forest/25 z-0"
       />
-      <Navbar />
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-1 min-h-[calc(100vh-44px)]">
-          <div className="max-w-[880px] mx-auto px-5 lg:px-8 py-8 lg:py-10">
-            <Outlet />
-          </div>
-        </main>
+      <div className="relative z-10">
+        <Navbar />
+        <div className="flex">
+          <Sidebar />
+          <main className="flex-1 min-h-[calc(100vh-44px)]">
+            <div className="max-w-[880px] mx-auto px-5 lg:px-8 py-8 lg:py-10">
+              <Outlet />
+            </div>
+          </main>
+        </div>
       </div>
     </div>
   )
